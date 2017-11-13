fd2bbe675d631ac78078b0b65278e9daf2b24104
User <User@DESKTOP-SAOE1G6>
2017-11-13 10:42:01 -0500

    Changes made to incorporate login script with front-end design. Changes made with the files from Monday.

---
+++ Model/logout_error.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+Purpose: This page displays a logout error message.
+Author: Kat Farley
+Date: 11/13/2017
+-->
+ 
+<html>
+  <!-- head section -->  
+  <head>
+      <title>Login to Bookshare</title> 
+  </head> 
+  <!-- body section -->
+  <body>
+      <main>
+          <h1>Logout Error</h1>
+          <p>There was an error logging out.</p>
+      
+          
+      </main>
+      
+  </body>
+    
+    
+    
+</html>

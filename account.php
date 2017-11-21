1de619da65e4148a57555803ef8c4b24947d8fa9
doirons2530 <33270131+doirons2530@users.noreply.github.com>
2017-11-21 17:39:03 -0500

    Design idea

---
+++ account.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+    
+    <title>BookShare | Account</title>
+    <link href="images/Icon.png" rel="shortcut icon" type="image/x-icon" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <link href="CSS/Styles.css" rel="stylesheet">
+          
+    </head>
+
+    
+    <body id="account-body">
+   
+        <nav class="desktop-nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li class="logged-out"><a href="login.php">Login</a></li>
+                <li id="sign-up" class="logged-out"><a href="#">Sign-Up</a></li>
+                <li class="logged-in"><a href="account.php" class="active">My Profile</a></li>
+            <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+           
+        <div id="account-menu">
+            <h1>Preferences</h1>
+            <a href="#"><p>Option 1</p></a>
+            <a href="#"><p>Option 2</p></a>
+            <a href="#"><p>Option 3</p></a>
+            <a href="#"><p>Option 4</p></a>
+            <a href="#"><p>Option 5</p></a>
+        </div>
+
+        <div id="account-container">
+           <h1>Your active listings:</h1>
+            <table id="account-data">
+                <tr>
+                    <th>
+                        <td><img src="images/bookimage.jpg" alt="Book Icon" width="180" height="160" /></td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Book Title</p>
+                            <p>Book ISBN</p>
+                            <p>Type: Donate/Trade</p>
+                        </td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Contact</p>
+                            <p>Save For Later</p>
+                        </td>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <td><img src="images/bookimage.jpg" alt="Book Icon" width="180" height="160" /></td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Book Title</p>
+                            <p>Book ISBN</p>
+                            <p>Type: Donate/Trade</p>
+                        </td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Contact</p>
+                            <p>Save For Later</p>
+                        </td>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <td><img src="images/bookimage.jpg" alt="Book Icon" width="180" height="160" /></td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Book Title</p>
+                            <p>Book ISBN</p>
+                            <p>Type: Donate/Trade</p>
+                        </td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Contact</p>
+                            <p>Save For Later</p>
+                        </td>
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <td><img src="images/bookimage.jpg" alt="Book Icon" width="180" height="160" /></td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Book Title</p>
+                            <p>Book ISBN</p>
+                            <p>Type: Donate/Trade</p>
+                        </td>
+                    </th>
+                    <th>
+                        <td>
+                            <p>Contact</p>
+                            <p>Save For Later</p>
+                        </td>
+                    </th>
+                </tr>
+            </table>
+        </div>
+        
+    </body>
+    
+</html>

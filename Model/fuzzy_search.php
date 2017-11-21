b83789bd9371a194200953b127ec8e5c3345f51b
Brian Oleniacz <brianoleniacz@gmail.com>
2017-11-21 18:42:44 -0500

    Deleted ADD FULLTEXT and wrong queries

--- Model/fuzzy_search.php
+++ Model/fuzzy_search.php
@@ -15,22 +15,10 @@
     
     try {
         
-        $index_books_table = "ALTER TABLE Books "
-                . "ADD FULLTEXT KEY (title, author, `ISBN-10`, `ISBN-13`);";
-                
-        $statement = $db->prepare($index_books_table);
-        $statement->execute(); 
-        
-        $better_query = "SELECT * FROM Books "
-                . "WHERE CONCAT_WS(' ', title, author, `ISBN-10`, `ISBN_13`) "
-                . "LIKE :search " ;
-        
         $query = "SELECT * FROM Books "
                 . "WHERE MATCH(title, author, `ISBN-10`, `ISBN-13`) "
                 . "AGAINST (:search)" ;
-        
-       $search = "%" . $search . "%";
-        
+
        $statement = $db->prepare($query);
        $statement->bindValue(':search', $search);
        $statement->execute();     

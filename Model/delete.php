55d97a39c52e71ef2d954232db3360c5fef33172
cthomas57 <33268553+cthomas57@users.noreply.github.com>
2017-12-04 16:26:52 -0500

    updated delete.php to work with profile.php

--- Model/delete.php
+++ Model/delete.php
@@ -1,13 +1,17 @@
 <?php
-$conn=mysqli_connect("localhost","root","root","test");
+$con=mysqli_connect("localhost","root","root","bookshare");
 // Check connection
 if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$id = $_GET['id']; 
+$id = (int)$_GET['id'];
 
-mysqli_query($conn,"DELETE FROM books WHERE id='".$id."'");
-mysqli_close($conn);
+$update = $con->prepare("DELETE FROM books WHERE book_id = '$id'");
+$update->bind_param('i', $id);
+$update->execute();
+$update->close();
+
 header("Location: ../profile.php");
-?> 
+
+?>

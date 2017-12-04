f0ae438e8540330916bcdd0bee454f98d4123616
User <User@DESKTOP-SAOE1G6>
2017-12-04 10:52:46 -0500

    Since this page is incomplete, I was not able to add working logging capability. Instead, I added comments for adding logging functionality for the handling author of this page. (KaF 12/4)

--- Model/request.php
+++ Model/request.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'Model/Database.class.php';
+require_once 'log_to_file.php';
 
 $to_name; //TODO first name from db
 $to_email; //TODO email from db
@@ -12,9 +13,23 @@ $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $requester_email \r\n";
 if (mail($to_email, $subject, $message, $headers)) {
     //TODO flat file log
+    // UPDATE: Please use this to log the results for both fails and successes, ensuring that usernames of sender/receiver and List Item ID are incorporated (KaF 12/4)
+
+    // Create log message with data and username
+    // $message = 'Mail sent to ' . $to_name;
+			
+    // Call function to log results
+    // logToFile('requestResultsFile.txt', $message);
+		   
+    
     include 'email_sent.php';
 } else {
     // TODO error log?
+        // Create log message with data and username
+    // $message = 'Mail from ' . $to_name . 'failed to send.';
+			
+    // Call function to log results - if desired, change this to a separate error file or use built-in error_log() function of PHP
+    // logToFile('requestResultsFile.txt', $message);
     include 'email_fail.php';
 }
 

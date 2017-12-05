3d07b4161befe2b67da6b7566b0ba00e8b0ee945
Brian Oleniacz <brianoleniacz@gmail.com>
2017-12-05 04:16:15 -0500

    fixed require_once quotes bug
    require_once 'database.php' with quotes
    Changed Chris's code to use Kat's database connection

--- Model/delete.php
+++ Model/delete.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost","root","root","bookshare");
+/*
+$con=mysqli_connect("localhost","root","","bookshare");
 // Check connection
 if (mysqli_connect_errno()) {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
@@ -11,7 +12,28 @@ $update = $con->prepare("DELETE FROM books WHERE book_id = '$id'");
 $update->bind_param('i', $id);
 $update->execute();
 $update->close();
+*/
 
-header("Location: ../profile.php");
 
-?>
+require_once 'database.php';
+
+$book_ID = filter_input(INPUT_GET, 'id');
+echo "book id is $book_ID";
+
+try {
+
+        $query = "DELETE FROM books"
+              . " WHERE book_ID = :book_ID; ";
+       $statement = $db->prepare($query);
+       $statement->bindValue(':book_ID', $book_ID);
+       $statement->execute();
+       $statement->closeCursor();
+
+    } catch (PDOException $ex) {
+        $msg = $ex->getMessage();
+        echo "error: " . $msg;
+        exit();
+    }
+
+// header("Location: ../profile.php");
+    ?>

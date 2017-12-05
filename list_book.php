5900b3df58c34c9c9691dd9d64a742973c7ed51c
jervinao <jervinao@gmail.com>
2017-12-05 11:48:38 -0500

    added id and class attributes for styling

--- list_book.php
+++ list_book.php
@@ -12,13 +12,14 @@ Date: 12/3/17
         <title>List A Book</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
-        <link href="CSS/styles" rel="stylesheet">
+        <link href="CSS/styles.css" rel="stylesheet">
         <style>
             #options {
                 display: none;
             }
         </style>
         <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+		<script src="JS/stopFlash.js"></script>
         <script>
             $(document).ready(function() {
             $.getScript("JS/global_search.js");
@@ -27,14 +28,40 @@ Date: 12/3/17
 
     </head>
 
-    <body>
+    <body id="listBook-body" style="visibility: hidden;" onload="js_Load()"> <!-- Fixes the weird split second flash of unstyled page - Forces everything hidden, then visible once all loaded -->
+		
+		<?php 
+         
+        // Check if a session is open before trying to start
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
 
-        <div id ="search">
-            <input type="text" id="isbnSearch" placeholder="ISBN">
-            <button onclick="lookup()" type="submit">Search ISBN</button>
+        // If the user is logged in, display logged in header
+        // NOTE: Alternatively, we can make one header file and control display inside of it
+        if(isset($_SESSION['valid_user'])) {
+            include 'header_loggedin.php'; 
+        }
+
+        // If the user is logged out, display logged out header
+        else {
+            include 'header_loggedout.php';
+        }
+        ?>
+		
+		<h1>Add Listing</h1>
+		<p>
+			Search for the book using its ISBN number.<br>
+			The ISBN is usually found on the back cover of the book, near the barcode.
+		</p>
+		
+		<div id ="isbnSearch-div">
+			<input type="text" id="isbnSearch" placeholder="ISBN (10 or 13)">
+            <button class="button" onclick="lookup()" type="submit">Search</button>
         </div>
         
-        <ul>
+		
+        <ul id="ul">
             <div id="content"></div>
         </ul>
         
@@ -42,7 +69,7 @@ Date: 12/3/17
             <form id="listBook" action="TODO.php" method="post">
                 <fieldset>
                     <legend>
-                        Condition:
+                        <strong>Condition:</strong>
                     </legend>
                     <label>
                         <input type="radio" name="condition" value="New" required />Like New
@@ -66,7 +93,7 @@ Date: 12/3/17
                 <br />
                 <fieldset>
                     <legend>
-                        Loan Type:
+                        <strong>Loan Type:</strong>
                     </legend>
                     <label>
                         <input type="checkbox" name="loan_type" value="Free" />Free
@@ -86,10 +113,10 @@ Date: 12/3/17
                     <div id="checked"></div>
                 </fieldset><br />
                 <label>
-                    Notes:
+                    <strong>Notes:</strong>
                     <textarea id="notes" name="notes" rows="4" maxlength ="255"
                               placeholder="i.e. What you want to trade in return"></textarea>
-                <button type="submit" id="list">List This Book</button>
+                <button class="button" type="submit" id="list">List This Book</button>
             </form>
         </div>
         

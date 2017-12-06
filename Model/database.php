40fa087c9d1f5e44891646a8d2e7db7d76fc9068
Brian Oleniacz <brianoleniacz@gmail.com>
2017-12-05 23:12:57 -0500

    added verbose error reporting to PDO

--- Model/database.php
+++ Model/database.php
@@ -1,9 +1,9 @@
 <?php
-/* 
+/*
  * Purpose: This file establishes the database connection
  * Author: Kat Farley
  * Date: 11/3/2017
- * 
+ *
  * Zack's new test comment
  */
 
@@ -11,9 +11,17 @@ $dsn = 'mysql:host=localhost;dbname=bookshare';
 $username = 'root';
 $password = '';
 
-// Check for exceptions 
+
+/*
+ * Check for exceptions
+ *
+ * edit: add array(...PDO:ERROMODE_EXCEPTION)
+ * to constructor to make PDOs
+ * verbose with SQL syntax errors.
+ */
 try {
-    $db = new PDO($dsn, $username, $password);
+    $db = new PDO($dsn, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
 } // end try
 catch (PDOException $ex) {
     $error_message = $ex->getMessage();
